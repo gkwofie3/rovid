d2c829d976df598f912275846d60ecee57b26df8
gkwofie3 <gideonkwofie13@gmail.com>
2025-07-13 12:33:39 +0000

    removed unecessary files

--- rovid/settings.py
+++ rovid/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'rovid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rovid$rovid',
-        'USER': 'rovid',
-        'PASSWORD': 'padb.rOvO@2k20',
-        'HOST': 'rovid.mysql.pythonanywhere-services.com',
-        'PORT': '3306', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'rovid$rovid',
+#         'USER': 'rovid',
+#         'PASSWORD': 'padb.rOvO@2k20',
+#         'HOST': 'rovid.mysql.pythonanywhere-services.com',
+#         'PORT': '3306', 
+#     }
+# }
 
 
 # Password validation
